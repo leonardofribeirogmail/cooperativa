a0b61254b2e08937a2c69b5c3a61f49c8a562709
Leonardo <leonardofr.ribeiro@gmail.com>
2024-07-27 16:22:42 -0700

    [COOP-V002] - Redis funcionando corretamente. Faltam testes

--- src/main/java/com/example/cooperativa/CooperativaVotacaoApplication.java
+++ src/main/java/com/example/cooperativa/CooperativaVotacaoApplication.java
@@ -2,10 +2,11 @@ package com.example.cooperativa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
 public class CooperativaVotacaoApplication {
 
 	public static void main(String[] args) {
