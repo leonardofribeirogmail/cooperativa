b95682b7227fc4c9a8f9664a6a98e50969910913
Leonardo <leonardofr.ribeiro@gmail.com>
2024-07-29 16:04:26 -0700

    [COOP-V002] - Corrigido bug de cacheamento na tabela associados

--- src/main/java/com/example/cooperativa/repository/PautaRepository.java
+++ src/main/java/com/example/cooperativa/repository/PautaRepository.java
@@ -2,8 +2,6 @@ package com.example.cooperativa.repository;
 
 import com.example.cooperativa.model.Pauta;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PautaRepository extends JpaRepository<Pauta, Long> {
 }
--- src/main/java/com/example/cooperativa/repository/SessaoVotacaoRepository.java
+++ src/main/java/com/example/cooperativa/repository/SessaoVotacaoRepository.java
@@ -3,9 +3,5 @@ package com.example.cooperativa.repository;
 import com.example.cooperativa.model.SessaoVotacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 public interface SessaoVotacaoRepository extends JpaRepository<SessaoVotacao, Long> {
-    List<SessaoVotacao> findByFimBeforeAndEncerradaFalse(final LocalDateTime now);
 }
